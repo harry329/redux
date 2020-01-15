c39eba4f3b70cb9742341e042bda9cde6df387aa
harry <harsimrans329@gmail.com>
2020-01-14 22:47:04 -0600

    Multiple reducers added.

--- store/index.js
+++ store/index.js
@@ -32,12 +32,37 @@ function creatStore(reducer) {
 
 function todos(state =[], actions) {
     if(actions.type === 'ADD_TODO') {
-        state = state.concat(actions.toDo)
+        return state.concat(actions.toDo)
+    } else if (actions.type === 'REMOVE_TODO') {
+        return state.filter( (todo) => todo.id !== actions.id)
+    } else if (actions.type === 'TOGGLE_TODO') {
+        return state.map( (todo) => if (todo.id !== actions.id) ? todo : Object.assign({}, todo, complete : !todo.complete) )
+    } else {
+        return state
     }
-    return state
+
 }
 
-let store = creatStore(todos)
+function goals(state=[], action) {
+    switch (action.type) {
+        case 'ADD_GOAL' :
+            return state.concat(action.goal)
+        case 'REMOVE_GOAL' :
+            return state.filter((goal) => goal.id !== action.goal)
+        default:
+            return state
+    }
+}
+
+function app (state = {}, action) {
+    return {
+        todos : todos(state.todos, action),
+        goals : goals(state.goals, action)
+    }
+
+}
+
+let store = creatStore(app)
 store.subscribe( () => {
     console.log(" i am getting changed", store.getStore())
 })
@@ -46,7 +71,7 @@ store.dispatch( {
     type: 'ADD_TODO',
     toDo : {
         id: 1,
-        complet: false,
+        complete: false,
         name: 'Learn Redux'
     }
 })
